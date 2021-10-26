376d3fab8fe4f12fbfdd5965023d7b56837ebc2c
Kirill Dzhunkowski <dzhunkofffski@gmail.com>
2021-10-27 01:01:54 +0300

    start commit

---
+++ cmake-build-debug/CMakeFiles/my_unique_ptr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_unique_ptr.dir/main.cpp.o"
+  "my_unique_ptr"
+  "my_unique_ptr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_unique_ptr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/my_unique_ptr.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for my_unique_ptr.
+# This may be replaced when dependencies are built.
